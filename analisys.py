2c505463d20a1b4266967b1a422a7a52e81e863a
unknown <netofr@gmail.com>
2024-05-19 16:32:34 +0100

    updates

--- analisys.py
+++ analisys.py
@@ -1,11 +1,120 @@
-# For handling data frames.
+# -----------------------------------------------------------------------------------------------
+
+# Higher Diploma in Science in Computing - Data Analytics
+
+# -----------------------------------------------------------------------------------------------
+
+# Programming and Scripting - Iris 
+
+# * Setosa
+# * Versicolor
+# * Virginica
+
+# -----------------------------------------------------------------------------------------------
+
+# About this project:
+
+# -----------------------------------------------------------------------------------------------
+
+# Libraries:
+ 
+# -----------------------------------------------------------------------------------------------
+
+# Importing necessary libraries for data manipulation, visualization, and numerical operations:
+
+# Data frames [3]
 import pandas as pd
 
-# For data visualization.
+# Data visualization library [3]
 import seaborn as sns
 
-# For plotting.
+# Plotting [5]
 import matplotlib.pyplot as plt
 
-# For numerical arrays and random numbers.
+# Numerical arrays and random numbers [6]
 import numpy as np
+
+# -----------------------------------------------------------------------------------------------
+
+# Load Data:
+
+# -----------------------------------------------------------------------------------------------
+
+# Load Iris Dataset:
+
+# -----------------------------------------------------------------------------------------------
+
+# Load the dataset into a DataFrame [8]
+df = pd.read_csv("iris.csv")
+print(df)
+
+# -----------------------------------------------------------------------------------------------
+
+#          sepal_length  sepal_width  petal_length  petal_width  species
+# 0             5.1          3.5           1.4          0.2       setosa
+# 1             4.9          3.0           1.4          0.2       setosa
+# 2             4.7          3.2           1.3          0.2       setosa
+# 3             4.6          3.1           1.5          0.2       setosa
+# 4             5.0          3.6           1.4          0.2       setosa
+# ..            ...          ...           ...          ...         ...
+# 145           6.7          3.0           5.2          2.3       virginica
+# 146           6.3          2.5           5.0          1.9       virginica
+# 147           6.5          3.0           5.2          2.0       virginica
+# 148           6.2          3.4           5.4          2.3       virginica
+# 149           5.9          3.0           5.1          1.8       virginica
+
+# -----------------------------------------------------------------------------------------------
+
+# Inspect Data:
+
+# -----------------------------------------------------------------------------------------------
+
+# Display the first 5 rows of the dataset.
+fiverows = df.head()
+print(fiverows)
+
+# -----------------------------------------------------------------------------------------------
+# sepal_length    sepal_width    petal_length    petal_width    species
+# 5.1             3.5            1.4             0.2            setosa
+# 4.9             3.0            1.4             0.2            setosa
+# 4.7             3.2            1.3             0.2            setosa
+# 4.6             3.1            1.5             0.2            setosa
+# 5.0             3.6            1.4             0.2            setosa
+
+# -----------------------------------------------------------------------------------------------
+
+# Display the last 5 rows of the dataset.
+lastrows = df.tail()
+print(lastrows)
+
+# -----------------------------------------------------------------------------------------------
+
+# sepal_length    sepal_width    petal_length    petal_width    species
+# 145             6.7            3.0             5.2            2.3       virginica
+# 146             6.3            2.5             5.0            1.9       virginica
+# 147             6.5            3.0             5.2            2.0       virginica
+# 148             6.2            3.4             5.4            2.3       virginica
+# 149             5.9            3.0             5.1            1.8       virginica
+
+# -----------------------------------------------------------------------------------------------
+
+# Display information about the dataset.
+inf = df.info()
+print(inf)
+
+# -----------------------------------------------------------------------------------------------
+
+# <class 'pandas.core.frame.DataFrame'>
+#  RangeIndex: 150 entries, 0 to 149
+#  Data columns (total 5 columns):
+#   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   sepal_length  150 non-null    float64
+#   1   sepal_width   150 non-null    float64
+#   2   petal_length  150 non-null    float64
+#   3   petal_width   150 non-null    float64
+#   4   species       150 non-null    object 
+#  dtypes: float64(4), object(1) 
+#  memory usage: 6.0+ KB
+
+# -----------------------------------------------------------------------------------------------
--- readme.md
+++ readme.md
@@ -1,5 +1,64 @@
 # Higher Diploma in Science in Computing - Data Analytics
+***
+## Programming and Scripting - Fisher’s Iris Dataset
+***
+This project focuses on accessing and analyzing the [Iris](https://github.com/corpuschris/pands-project/blob/master/iris.csv) dataset, a classic dataset frequently used in data science and machine learning.[1] By working with this dataset, we aim to showcase a range of skills, including data manipulation, statistical analysis, visualization, and programming techniques using [Python](https://www.python.org/) within the Jupyter Notebook environment.[2]
 
-# Author: Christiano ferreira
+### About this project:
+***
 
-# Programming and Scripting - Fischer's Iris
+This project involves the [Iris](https://github.com/corpuschris/pands-project/blob/master/iris.csv) dataset.
+
+![Iris](https://github.com/corpuschris/pands-project/assets/159485788/e79c61f7-7b01-4dec-aa58-662734067af9)
+
+[Fischer's Iris Data](https://archive.ics.uci.edu/dataset/53/iris) UCI Machine Learning Repository: Iris Data Set. [3]
+
+### Getting Started:
+***
+
+I used [openincolab](https://openincolab.com/) to generate the following clickable link. It opens the [`analysis.ipynb`](https://github.com/corpuschris/pands-project/blob/master/analysis.ipynb) Jupyter Notebook in [Google Colab](https://colab.research.google.com/)
+
+<a target="_blank" href="https://colab.research.google.com/github/CarlosRigueti/pands-project">
+  <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/>
+</a>
+
+***
+### License:
+***
+
+The Iris dataset is a widely used and publicly available dataset that is considered to be in the public domain. It was originally collected and introduced by the British statistician and biologist Ronald Fisher in 1936.
+
+* **UCI Machine Learning Repository:**
+  The Iris dataset is hosted on the UCI Machine Learning Repository, a popular resource for machine learning datasets.
+  Link: [UCI Machine Learning Repository - Iris Dataset](https://archive.ics.uci.edu/dataset/53/iris)
+* **scikit-learn:**
+  The Iris dataset is included as one of the built-in datasets in the scikit-learn library, a popular machine learning library for Python.
+  You can load the Iris dataset directly from scikit-learn using the `load_iris()` function.
+  Link: [scikit-learn - Iris Dataset](https://scikit-learn.org/stable/datasets/toy_dataset.html#iris-dataset)
+* **Kaggle:**
+  The Iris dataset is available on Kaggle, a platform for data science competitions and datasets.
+  Link: [Kaggle - Iris Dataset](https://www.kaggle.com/uciml/iris)
+* **GitHub:**
+  Many repositories on GitHub contain the Iris dataset, often as part of educational materials or example code.
+  You can search GitHub for repositories containing the Iris dataset using keywords like "Iris dataset".
+
+This dataset is licensed under a [Creative Commons Attribution 4.0 International](https://creativecommons.org/licenses/by/4.0/legalcode) (CC BY 4.0) license.
+***
+
+### References:
+***
+[1] About READMEs. Available in: https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-readmes. [Accessed 06 May 2024].
+
+[2] Fischer's Iris dataset. Available in: https://archive.ics.uci.edu/dataset/53/iris. [Accessed 07 May 2024].
+
+[3] The iris data set: In search of the source of virginica. Available in: https://academic.oup.com/jrssig/article/18/6/26/7038520?login=false. [Accessed 07 May 2024].
+
+[4] Kaggle: https://www.kaggle.com/ [Accessed 07 May 2024].
+
+[5] Scikit-learn: https://scikit-learn.org/stable/ [Accessed 07 May 2024].
+
+***
+
+#### Author: Christiano Ferreira
+
+* **Christiano Ferreira** - I am currently studying at [ATU](https://www.atu.ie/) - Atlantic Technological University in the course Higher Diploma in Computing in Data Analysis. To contact me for more information, please reach out on GitHub or connect with me on [LinkedIn](https://www.linkedin.com/in/christiano-ferreira/).
