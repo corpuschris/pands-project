fd01b897ac3c2aaaf3930bab52c947e39591ee8b
unknown <netofr@gmail.com>
2024-05-20 14:06:15 +0100

    updates

---
+++ analysissummary.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------------------
+
+#Higher Diploma in Science in Computing - Data Analytics
+
+#-------------------------------------------------------------------------------------------
+
+#Author: Christiano Ferreira
+
+#-------------------------------------------------------------------------------------------
+
+#Programming and Scripting - Fischer's Iris.
+
+#This project centers on accessing and analyzing the Iris dataset, a well-known dataset frequently employed in data science and machine learning . 
+#Through this dataset, we aim to demonstrate a variety of skills, including data manipulation, statistical analysis, visualization, and programming techniques using Python within the Jupyter Notebook environment .
+#The Iris flower dataset is a multivariate dataset introduced by British statistician and biologist Ronald Fisher in his 1936 paper, "The use of multiple measurements in taxonomic problems." 
+#It is also referred to as Anderson's Iris dataset because Edgar Anderson collected the data to measure the morphological variation of Iris flowers from three related species: Setosa, Versicolor and Virginica.
+
+#-------------------------------------------------------------------------------------------
+
